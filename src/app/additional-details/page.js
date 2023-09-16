32cbc8940e53fe31bde691ee498bdf97fc568206
Josh Nguyen <joshuahnguyen@gmail.com>
2023-09-16 13:52:00 -0400

    finalized additional details page by adding functionality in preparation for cohere.ai, and adjusting CSS elements

--- src/app/additional-details/page.js
+++ src/app/additional-details/page.js
@@ -10,35 +10,37 @@ const SCROLLS = [
 const READ = [
     "Left to Right",
     "Right to Left"
-    
 ]
 
 export default function Home() {
+    const [wordCount, setWordCount] = useState(300) 
     const [scrollPreference, setScrollPreference] = useState(SCROLLS[0])
     const [readPreference, setReadPreference] = useState(READ[0])
 
-    const scrollSelection = (preference) => {
 
+    const scrollSelection = (preference) => {
+        setScrollPreference(preference)
     }
 
-    const readSelection = () => {
-
+    const readSelection = (preference) => {
+        setReadPreference(preference)
     }
     return (
-        <main className="flex bg-bgcolor min-h-screen w-full flex-col items-center justify-between p-24">
-            <div className="float-left text-left w-2/4 text-4xl flex flex-wrap">
+        <main className="flex items-start bg-bgcolor min-h-screen w-full flex-wrap  justify-between p-24">
+            <div className=" w-2/4 text-4xl flex flex-wrap">
                 Additional Details
+                <p className="text-xl">(This is optional)</p>
             </div>
             <div className="float-left w-2/4">
-                <p>
-                    Word limit
+                <p className="text-[20px] my-3">
+                    Word limit ({wordCount})
                 </p>
-                <input type="text" className=" w-full p-2 m-1 border-2 rounded-full  border-htnblack border-md"></input>
-                <p>
+                <input onChange={(e) => setWordCount(e.target.value)} className=" accent-htnblack  w-full" type="range" min="100" max="500" defaultValue={wordCount}></input>
+                <p className="text-[20px] my-4">
                     Scrolling direction:
                 </p>
                 <TwoChoiceSelector pair={SCROLLS} preference={scrollSelection}></TwoChoiceSelector>
-                <p>
+                <p className="text-[20px] my-4">
                     Reading from:
                 </p>
                 <TwoChoiceSelector pair={READ} preference={readSelection}></TwoChoiceSelector>
